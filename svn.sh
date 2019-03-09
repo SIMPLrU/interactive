42c147bd237a75569781ca5b388a4c816585a2ed
SIMPLrU <viet@simplru.com>
2019-03-09 11:27:50 +0100

    initial commit

---
+++ svn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Stops Script on First Error
+set -e
+echo Welcome to the Jungle
+
+# ====EXIT script====
+  exit 2    # Misuse of shell builtins (according to Bash documentation)
+  exit 0    # Success
+  exit 1    # General errors, Miscellaneous errors, such as "divide by zero" and other impermissible operations
