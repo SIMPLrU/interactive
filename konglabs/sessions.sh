7151ffb13dfe6091f1d00894f4ac9d96c76c545d
Viet <viet@simplru.com>
2019-06-27 21:52:37 +0300

    modified reload to specify parameters
    -e 'admin_gui_session_conf' 
    -e 'enforce_rbac' 
    -e 'admin_gui_aut

--- konglabs/sessions.sh
+++ konglabs/sessions.sh
@@ -5,9 +5,7 @@
 KONG_ENFORCE_RBAC=on \
 KONG_ADMIN_GUI_AUTH=basic-auth \
 KONG_ADMIN_GUI_SESSION_CONF='{ "secret":"admin_secret", "cookie_secure":false, "storage":"kong", "cookie_name":"kookie", "cookie_samesite":"off" }' \
-kong reload kong.conf --vv
-
-# | grep -e 'admin_gui_session_conf'
+kong reload kong.conf --vv | grep -e 'admin_gui_session_conf' -e 'enforce_rbac' -e 'admin_gui_auth'
 
 # ====EXIT script====
 exit 2  	# Misuse of shell builtins (according to Bash documentation)
