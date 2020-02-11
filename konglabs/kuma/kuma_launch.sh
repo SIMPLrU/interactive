cbb96fdbd5c5b47620d6a6b847710e2e3f1881f5
vietphan <viet@simplru.com>
2020-02-11 16:40:34 +0300

    kuma_launch.sh init commit

---
+++ konglabs/kuma/kuma_launch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# DEPLOY K3S SERVER
+nohup k3s server --docker > /dev/null 2>&1 &
+    
+echo "installing your kubernetes cluster...please wait"
+
+sleep 10s
+
+# Create Alias 
+alias kubectl="k3s kubectl"
+
+# WAIT FOR SERVICES
+
+# coredns #? PASSED 
+kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/coredns
+
+echo "coredns is ready"
+
+# metrics-server #? PASSED
+kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/metrics-server
+
+echo "metrics-server is ready"
+
+# local-path-provisioner #? PASSED
+kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/local-path-provisioner
+
+echo "local-path-provisioner"
+
+# traefik #? PASSED
+kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/traefik
+
+echo "traefik is ready"
+
+# helm-install
+# svclb-traefik
+
+
+# Display Cluster Info
+kubectl cluster-info
+
+# Verify Cluster is working
+kubectl get pods --all-namespaces
