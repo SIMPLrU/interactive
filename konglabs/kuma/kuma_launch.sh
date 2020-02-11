2bf6605686fbdc02d8564f747cf30ccb0b9a783f
vietphan <viet@simplru.com>
2020-02-11 17:00:45 +0300

    add scripts parameters

--- konglabs/kuma/kuma_launch.sh
+++ konglabs/kuma/kuma_launch.sh
@@ -5,36 +5,42 @@
 # DEPLOY K3S SERVER
 nohup k3s server --docker > /dev/null 2>&1 &
     
-echo "installing your kubernetes cluster...please wait"
+echo "Installing your kubernetes cluster...please wait"
 
 sleep 20s
+
+echo "Deploying your cluster ..."
+echo 2s 
+
+# Create Alias 
+alias kubectl="k3s kubectl"
+sleep 5
 
 # Display Cluster Info
 kubectl cluster-info
 
-sleep 3s
-
-# Create Alias 
-alias kubectl="k3s kubectl"
-
 # WAIT FOR SERVICES
+echo  "Kubernetes cluster starting ..."
 
 # coredns #? PASSED 
+echo "installing coredns"
 kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/coredns
 
 echo "coredns is ready"
 
 # metrics-server #? PASSED
+echo "installing metric-server"
 kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/metrics-server
-
 echo "metrics-server is ready"
 
 # local-path-provisioner #? PASSED
-kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/local-path-provisioner
 
-echo "local-path-provisioner"
+echo "installing local-path-provisioner"
+kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/local-path-provisioner
+echo "local-path-provisioner is ready"
 
 # traefik #? PASSED
+echo "installing traefik "
 kubectl wait --timeout=200s --for=condition=Available -n kube-system deployment/traefik
 
 echo "traefik is ready"
@@ -42,7 +48,7 @@ echo "traefik is ready"
 # helm-install
 # svclb-traefik
 
-
+clear 
 # Display Cluster Info
 kubectl cluster-info
 
