d81c0d547066f7e9082c1cacd31f69b209310d5b
vietphan <viet@simplru.com>
2020-05-12 21:41:40 +0300

    add comments and descriptions

--- konglabs/workshop_se/launch-dev-portal.sh
+++ konglabs/workshop_se/launch-dev-portal.sh
@@ -5,11 +5,13 @@
 # launch-dev-portal.sh
 cd docker-compose && docker-compose -f workshop_bootstrap_dev_portal.yml up -d
 
+# Wait for containers to load
 echo "please wait..."
 sleep 2s
 echo "Kong Enterprise installing..."
 sleep 7s
 
+# Verify Kong Enterprise is installed otherwise wait
 n=0
 until [ "$n" -ge 5 ]
 do
@@ -19,9 +21,11 @@ do
    sleep 3s
 done
 
+# Install Mocking Plugin
+echo "Mocking Plugin installing ..."
 docker cp temp/kong-plugin-mocking/kong/plugins/mocking/  kong-ent:/usr/local/share/lua/5.1/kong/plugins
 docker exec -ti kong-ent /bin/sh -c "KONG_PLUGINS='bundled,mocking' kong reload" && rm -Rf temp
-echo "Mocking Plugin ready ..."
 
+# Provide environment parameters
 echo "****Kong Environment*****"
 echo "Kong Manager: $KONG_GUI"
