143b0e442e6ba4ff3ae3a9ca32cfa648a70715a4
vietphan <viet@simplru.com>
2020-05-12 10:18:18 +0300

    Add Mocking plugin to script

---
+++ konglabs/workshop_se/launch-dev-portal-mocking.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# stops script on first error
+# set -e
+
+cd docker-compose
+docker-compose -f workshop_bootstrap_dev_portal.yml up -d
+
+echo "please wait..."
+sleep 1s
+echo "Kong Enterprise installing..."
+sleep 5s
+
+echo "Installing Mocking Plugin"
+docker cp tmp/kong-plugin-mocking/kong/plugins/mocking/  kong-ent:/usr/local/share/lua/5.1/kong/plugins
+docker exec -ti kong-ent /bin/sh -c "KONG_PLUGINS='bundled,mocking' kong reload"
+cd .. && rm -Rf tmp
+
+sleep 1s
+echo "Access Kong Manager here"
+echo $KONG_GUI 
+
+
+
+
