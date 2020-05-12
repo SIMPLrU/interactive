4b02a553a2a0dc81cecd210d6a942ba3e7a07738
vietphan <viet@simplru.com>
2020-05-12 20:02:02 +0300

    add wait and rety to script

--- konglabs/workshop_se/launch-dev-portal.sh
+++ konglabs/workshop_se/launch-dev-portal.sh
@@ -7,7 +7,17 @@ cd docker-compose && docker-compose -f workshop_bootstrap_dev_portal.yml up -d
 echo "please wait..."
 sleep 1s
 echo "Kong Enterprise installing..."
-sleep 10s
+sleep 7s
+
+n=0
+until [ "$n" -ge 5 ]
+do
+   http get :8001 && break # substitute your command here
+   echo "a few more moments ..."
+   n=$((n+1))
+   sleep 3
+done
+
 
 echo "Installing Mocking Plugin"
 docker cp temp/kong-plugin-mocking/kong/plugins/mocking/  kong-ent:/usr/local/share/lua/5.1/kong/plugins
