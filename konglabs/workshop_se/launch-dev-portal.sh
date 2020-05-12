b3d6c3a998666ece09f8043adb18accd05892cbe
vietphan <viet@simplru.com>
2020-05-12 21:34:11 +0300

    simplify script

--- konglabs/workshop_se/launch-dev-portal.sh
+++ konglabs/workshop_se/launch-dev-portal.sh
@@ -2,6 +2,7 @@
 # stops script on first error
 # set -e
 
+# launch-dev-portal.sh
 cd docker-compose && docker-compose -f workshop_bootstrap_dev_portal.yml up -d
 
 echo "please wait..."
@@ -12,16 +13,15 @@ sleep 7s
 n=0
 until [ "$n" -ge 5 ]
 do
-   http get :8001 2>/dev/null && break # substitute your command here
+   http get :8001 2>/dev/null && echo "Kong Enterprise ready" && break # substitute your command here
    echo "just a few more moments ..."
    n=$((n+1))
    sleep 3s
 done
 
-
-echo "Mocking Plugin installing ..."
 docker cp temp/kong-plugin-mocking/kong/plugins/mocking/  kong-ent:/usr/local/share/lua/5.1/kong/plugins
 docker exec -ti kong-ent /bin/sh -c "KONG_PLUGINS='bundled,mocking' kong reload" && rm -Rf temp
+echo "Mocking Plugin ready ..."
 
-echo "**** Kong-Environment*****"
+echo "****Kong Environment*****"
 echo "Kong Manager: $KONG_GUI"
