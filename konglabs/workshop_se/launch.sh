551fe2e6f213a28026682b8401974d59594f8f45
vietphan <viet@simplru.com>
2020-06-09 10:49:16 +0300

    add Export RUNNER_ALLOW_RUNASROOT=1 variable

--- konglabs/workshop_se/launch.sh
+++ konglabs/workshop_se/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # stops script on first error
 # set -e
-export RUNNER_ALLOW_RUNASROOT=1
+export RUNNER_ALLOW_RUNASROOT=1    
 
 cd docker-compose
 docker-compose -f workshop_bootstrap.yml up -d
