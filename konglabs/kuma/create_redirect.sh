e76d1ae3b02e6d35a2ff2d16282bfd0336602756
vietphan <viet@simplru.com>
2020-10-06 10:37:34 +0300

    remove redirect.sh

--- konglabs/kuma/create_redirect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#http PUT "https://dev-513727-admin.okta.com/oauth2/v1/clients/0oa4giun5hZXCeaQB357" Authorization:SSWS\ 00wuBqKCAd9F8crLG3NXDB2P2FleKUADL8o8tBLdCv < body.json
-
-
-http PUT "https://dev-513727-admin.okta.com/api/v1/apps/0oa4giun5hZXCeaQB357" Authorization:SSWS\ 00wuBqKCAd9F8crLG3NXDB2P2FleKUADL8o8tBLdCv < app.json
