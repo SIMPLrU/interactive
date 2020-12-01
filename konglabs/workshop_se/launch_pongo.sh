6d5728f81cef6ff239471e7335e31346f033ae31
vietphan <viet@simplru.com>
2020-12-01 17:54:48 +0300

    add launch_pongo.sh

--- konglabs/workshop_se/launch_pongo.sh
+++ konglabs/workshop_se/launch_pongo.sh
@@ -3,8 +3,15 @@
 # set -e
 
 mkdir -p ~/.local/bin
+# export variables
 export KONG_VERSION=2.1.3.1
 export POSTGRES=9.6
 export PATH=~/.local/bin/:$PATH
+
+# set symlink
 ln -s $(realpath kong-pongo/pongo.sh) ~/.local/bin/pongo
+
+# run Pongo
+pongo run
+
 
