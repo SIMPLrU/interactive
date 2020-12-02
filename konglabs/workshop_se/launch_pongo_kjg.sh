e19d2960556f1751c4717f824c9c52de09ee28ae
vietphan <viet@simplru.com>
2020-12-02 11:28:49 +0300

    add ENV to pongo_kjg.sh

---
+++ konglabs/workshop_se/launch_pongo_kjg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# stops script on first error
+# set -e
+
+mkdir -p ~/.local/bin
+# export variables
+export KONG_VERSION=2.1.3.1
+export POSTGRES=9.6
+export PATH=~/.local/bin/:$PATH
+
+# set symlink
+ln -s $(realpath kong-pongo/pongo_kjg.sh) ~/.local/bin/pongo
+
+# run Pongo
+cd kong-plugin
+pongo run
+
+
