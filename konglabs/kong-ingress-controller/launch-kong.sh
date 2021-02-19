be637535dab0f735fff35cd7e6ef0bdbdd4c877a
vietphan <viet@simplru.com>
2021-02-19 15:14:57 +0300

    new license

--- konglabs/kong-ingress-controller/launch-kong.sh
+++ konglabs/kong-ingress-controller/launch-kong.sh
@@ -7,5 +7,5 @@
 ############################################################################
 
 kubectl create namespace kong
-kubectl create secret generic kong-enterprise-license --from-file=license=./license -n kong
+kubectl create secret generic kong-enterprise-license --from-file=license=./license.json -n kong
 kubectl apply -f demokong-enterprise-0.10.yaml
