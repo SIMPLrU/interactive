6a867a3d79397585a5442fc130077e23c7d6f61d
vietphan <viet@simplru.com>
2021-03-04 10:37:24 +0300

    create new rbac script

---
+++ konglabs/workshop_se/launch_rbac.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# stops script on first error
+# set -e
+# start docker
+
+systemctl is-active docker.service || systemctl start docker
+cd docker-compose && docker-compose -f workshop_bootstrap_rbac.yml up -d
+
+
+echo "please wait..."
+sleep 1s
+echo "Kong Enterprise installing..."
+sleep 5s
+
+while true
+do
+    STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" localhost:8001/status)
+    if [ $STATUS_CODE -eq 200 ]; then
+	http get :8001 && echo "OK" 
+	break
+    else
+	echo "Kong not started yet - status: $STATUS_CODE. Please wait"
+        sleep 5s
+    fi
+done
