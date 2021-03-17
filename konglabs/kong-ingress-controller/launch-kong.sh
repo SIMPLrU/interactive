fc5fef186be042c7cad2ea9334a86c0f4ce41343
vietphan <viet@simplru.com>
2021-03-17 12:02:21 +0300

    modify k8 yaml files + add version variables

--- konglabs/kong-ingress-controller/launch-kong.sh
+++ konglabs/kong-ingress-controller/launch-kong.sh
@@ -6,8 +6,8 @@
 # DEPLOY KONG FOR KUBERNETES
 ############################################################################
 
-KONG_EE_VERSION=2.0.4.1-alpine
-KIC_VERSION=1.1
+export KONG_EE_VERSION=2.0.4.1-alpine
+export KIC_VERSION=1.1
 kubectl create namespace kong
 kubectl create secret generic kong-enterprise-license --from-file=license=./license.json -n kong
 kubectl apply -f demokong-enterprise.yaml
