4d5b6a514552c3a0359413006953f312a217b671
vietphan <viet@simplru.com>
2021-03-17 14:43:28 +0300

    latest yml

--- konglabs/kong-ingress-controller/launch-kong.sh
+++ konglabs/kong-ingress-controller/launch-kong.sh
@@ -6,9 +6,9 @@
 # DEPLOY KONG FOR KUBERNETES
 ############################################################################
 
-export KONG_EE_VERSION=2.0.4.1-alpine
+export KONG_EE_VERSION=2.2.0.0-alpine
 export KIC_VERSION=1.1
 
 kubectl create namespace kong
 kubectl create secret generic kong-enterprise-license --from-file=license=./license.json -n kong
-kubectl apply -f demokong-enterprise-0.10.yaml
+kubectl apply -f demokong-enterprise.yaml
