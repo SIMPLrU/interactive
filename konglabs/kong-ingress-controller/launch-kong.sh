1e2cd88749bfaae8644d52ebdb6f24f0e1c1a255
vietphan <viet@simplru.com>
2021-03-17 12:50:54 +0300

    revert back to 0.10 yaml

--- konglabs/kong-ingress-controller/launch-kong.sh
+++ konglabs/kong-ingress-controller/launch-kong.sh
@@ -11,4 +11,4 @@ export KIC_VERSION=1.1
 
 kubectl create namespace kong
 kubectl create secret generic kong-enterprise-license --from-file=license=./license.json -n kong
-kubectl apply -f demokong-enterprise.yaml
+kubectl apply -f demokong-enterprise-0.10.yaml
