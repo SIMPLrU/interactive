358dda42db2c1891ace23d5da02ef25cef8a5d96
vietphan <viet@simplru.com>
2021-03-18 13:10:57 +0300

    add status check

--- konglabs/workshop_se/launch_rbac.sh
+++ konglabs/workshop_se/launch_rbac.sh
@@ -14,14 +14,14 @@ sleep 1s
 echo "Kong Enterprise installing..."
 sleep 5s
 
-# while true
-# do
-#    STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" localhost:8001/status Kong-Admin-Token:kong_admin)
-#    if [ $STATUS_CODE -eq 200 ]; then
-#	http get :8001 Kong-Admin-Token:kong_admin && echo "OK" 
-#	break
-#   else
-#	echo "Kong not started yet - status: $STATUS_CODE. Please wait"
-#        sleep 5s
-#    fi
-# done
+while true
+do
+    STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" localhost:8001/status)
+    if [ $STATUS_CODE -eq 401 ]; then
+	http get :8001 Kong-Admin-Token:kong_admin && echo "OK" 
+	break
+   else
+	echo "Kong not started yet - status: $STATUS_CODE. Please wait"
+        sleep 5s
+    fi
+ done
