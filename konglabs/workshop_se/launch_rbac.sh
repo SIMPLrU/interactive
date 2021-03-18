f39f299900fbd1f18d71eb5db5769b00eb0321e2
vietphan <viet@simplru.com>
2021-03-18 11:17:50 +0300

    modify rbac script with RBAC token

--- konglabs/workshop_se/launch_rbac.sh
+++ konglabs/workshop_se/launch_rbac.sh
@@ -18,7 +18,7 @@ while true
 do
     STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" localhost:8001/status)
     if [ $STATUS_CODE -eq 200 ]; then
-	http get :8001 && echo "OK" 
+	http get :8001 Kong-Admin-Token:kong_admin && echo "OK" 
 	break
     else
 	echo "Kong not started yet - status: $STATUS_CODE. Please wait"
